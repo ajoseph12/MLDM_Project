2544dc90292df9f5096cf386bba5a0c9e4c42c39
ajoseph12 <allwyn_don32@yahoo.com>
2018-10-05 11:41:35 +0200

    GANZ

---
+++ Ganz/support.py
@@ -0,0 +1,18 @@
+import torch 
+from torch import nn, optim
+from torch.autograd.variable import Variable
+from torchvision import transforms, datasets
+
+
+DATA_FOLDER = './torch_data/VGAN/MNIST'
+
+def minst_data():
+
+	compose = transforms.Compose([transforms.ToTensor(), 
+		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+	out_dir = '{}/dataset'.fromat(DATA_FOLDER)
+	return datasets.MNIST(root = out_dir, train = True, transform = compose,
+		download = True)
+
+
