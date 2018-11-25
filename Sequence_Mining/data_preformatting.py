d1d8f1a0199022b9a857efcf0f0c7fcc4832c863
nisalup <nisalup@gmail.com>
2018-11-25 12:25:59 +0100

    seq mining intiial commit with patterns mined

---
+++ Sequence_Mining/data_preformatting.py
@@ -0,0 +1,36 @@
+#preprocess input freeman codes
+import pandas as pd
+import os
+
+script_dir = os.path.dirname(__file__)
+
+#import freeman code for drawn all images
+freeman_codes = pd.read_hdf('df_freemancode.hdf')
+
+db = []
+
+#mine the codes from 0 to 9
+for x in range(10):
+    db = []
+    temp_class = freeman_codes.loc[freeman_codes['Label'] == float(x)]
+
+    # generate input array from pandas dataframe
+    for index, row in temp_class.iterrows():
+        db.append(row["Freeman Code"][0].tolist())
+
+    with open(os.path.join(script_dir, "./input/input_patterns_for_" + str(x) + ".txt"), "w") as text_file:
+        for item in db:
+            item1 = str.replace(str(item), ',', '')
+            item2 = str.replace(item1, '[', '')
+            item3 = str.replace(item2, ']', '')
+            print(item3 + ' -1 -2', file=text_file)
+
+
+
+
+
+
+
+#frequent_rec([], [(i, -1) for i in range(len(db))])
+
+#print(results)
