6fd9c1b49fc6e77659e508377a63718cc52c7600
ajoseph12 <allwyn_don32@yahoo.com>
2018-12-09 17:48:24 +0100

    Nisal

---
+++ Game/mini_sudoku.py
@@ -0,0 +1,27 @@
+
+"""
+- create empty matrix
+- fill up matrix w.r.t defined constraints
+- g
+
+"""
+
+
+## Dependencies
+import numpy as np 
+
+class Mini_sudoku(object):
+
+	def __init__(self, rows, cols, cons):
+
+		self.rows = rows
+		self.cols = cols
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
