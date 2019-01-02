6556deda5d2e2605211b47d06934a649c18bdcff
ajoseph12 <allwyn_don32@yahoo.com>
2019-01-02 11:44:49 +0100

    k=48

--- Numerical_Method/numerical.py
+++ Numerical_Method/numerical.py
@@ -18,7 +18,7 @@ class Numerical(object):
 
 	def __init__(self, image_input, dataset_path = '../Autoencoders/embeddings/embeddings_197_b.npy', 
 		relevant_idx_path = 'Numerical_Method/relevant_idx_197_b', 
-		ae_model_path = 'Autoencoders/model/demo_autoencoder_197_b.pkl', k = 49):
+		ae_model_path = 'Autoencoders/model/demo_autoencoder_197_b.pkl', k = 48):
 
 		"""
 		Objects instantiated on creating instance of the numerical class
